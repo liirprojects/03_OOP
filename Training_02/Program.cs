286d973a377622ce6c6ae6f67f12531728984a40
valeriiia2003 <leraspasibukhova@ukr.net>
2024-10-11 14:01:21 +0100

    Second task is completed

---
+++ Training_02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq.Expressions;
+
+namespace Training_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> alimalsList = new List<Animal>
+            {
+                new Cat(), new Dog()
+            };
+            AnimalSpeaking(alimalsList);
+
+
+            Animal animal = new Cat();
+            Cat cat = (Cat)animal;
+            cat.Speak();
+            Console.ReadKey();
+        }
+        static void AnimalSpeaking(List<Animal> alimalsList)
+        {
+            foreach (var animal in alimalsList)
+            {
+                animal.Speak();
+            }
+        }
+    }
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("Animal is speaking..");
+        }
+    }
+    class Cat :Animal
+    {
+        public override void Speak()
+        {
+            base.Speak();
+            Console.WriteLine("Meow");
+        }
+
+        public void Hello() { }
+    }
+    class Dog :Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+}
