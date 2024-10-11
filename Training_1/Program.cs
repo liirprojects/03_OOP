5d08f5bca6f8c5f2b0d2c37bcae6eeb1deb013f0
valeriiia2003 <leraspasibukhova@ukr.net>
2024-10-11 13:14:53 +0100

    First task is completed

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -1,10 +1,69 @@
-﻿namespace Training_1
+﻿using System;
+
+namespace Training_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount bankAccount1 = new BankAccount(10904690);
+            bankAccount1.TopUpMethod(200);
+            bankAccount1.WithDrawMoney(20);
+
+            Console.ReadKey();
         }
+    }
+    class BankAccount
+    {
+        private double balance;
+        private uint accountnumber;
+
+        private DateTime dateTime;
+
+        public BankAccount(uint account)
+        {
+            Console.WriteLine("Initial balance of the card : 0.00");
+            this.accountnumber = account;     
+
+            dateTime = DateTime.Now;
+        }
+
+        public double Balance
+        {
+            private set
+            {
+                if (value < -100)
+                    throw new ArgumentOutOfRangeException("Blance credit can not be less tham 100.00");
+                balance = value;
+            }
+            get {
+                return balance;
+            }
+        }
+
+        public void TopUpMethod(double amount)
+        {
+            if (amount > 0)
+            {
+                balance = balance + amount;
+
+                Console.WriteLine("Balance for {0} : [{1}]", dateTime, balance.ToString("F2"));
+            }
+            else
+                Console.WriteLine("Impossible to top up an amount with a negative or 0 values.");
+        }
+
+        public void WithDrawMoney(double amount)
+        {
+            if (amount <= balance && amount > 0)
+            {
+                balance = balance - amount;
+                Console.WriteLine("After withdrawing {0} -- balance for {1} : [{2}]", 
+                    amount.ToString("F2"), dateTime, balance.ToString("F2"));
+            }
+            else
+                Console.WriteLine("Balance is less than credit to withdraw or your values is negative");
+        }
+            
     }
 }
