5102d3d5f8dc0cc8d3983b2035c98ab58cbf34f9
valeriiia2003 <leraspasibukhova@ukr.net>
2024-10-15 14:06:45 +0100

    The fifth task is completed.

---
+++ Training_05/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Training_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            DrivingMethod(car);
+
+            Console.ReadKey();
+        }
+        static void DrivingMethod(IDrivable drivable)
+        {
+            drivable.Drive();
+        }
+    }
+    interface IDrivable
+    {
+        void Drive();
+    }
+    class Car : IDrivable
+    {
+        public void Drive()
+        {
+            Console.WriteLine("This is method for driving a car");
+        }
+    }
+    class Bicycle : IDrivable
+    {
+        public void Drive()
+        {
+            Console.WriteLine("This is method for driving a bicycle");
+        }
+    }
+}
